ca40540bcaf8e90bad45276a458c6b7452ec2a80
Parag Chimankar <141899674+Chimankarparag@users.noreply.github.com>
2026-01-30 13:24:24 +0530

    added previous year assignments

--- Previous year/2/signal.c
+++ Previous year/2/signal.c
@@ -1,6 +1,6 @@
 /***********************************************************/
 /*** Sample program demonstrating the sending of signals ***/
-/*** Last updated by Abhijit Das, 11-Jan-2024            ***/
+/*** Last updated by Abhijit Das, 13-Jan-2026            ***/
 /***********************************************************/
 
 /***********************************************************/
@@ -18,15 +18,35 @@
 #include <sys/wait.h>
 #include <signal.h>
 
+/* Customized sleep function */
+void mysleep ( )
+{
+   int i, t;
+
+   usleep(100000);
+   t = 5 + rand() % 5;
+   printf("+++ Parent: Going to sleep for %d seconds:", t);
+   fflush(stdout);
+   for (i=1; i<=t; ++i) {
+      sleep(1);
+      printf(" %d", i);
+      fflush(stdout);
+   }
+   usleep(250000);
+   printf("\n");
+}
+
 /* The signal handler for the child process */
 void childSigHandler ( int sig )
 {
    if (sig == SIGUSR1) {
-      printf("\n+++ Child : Received signal SIGUSR1 from parent...\n");
-      sleep(1);
+      printf("\n\t\t\t\t\t+++ Child : Received signal SIGUSR1 from parent...\n\n");
    } else if (sig == SIGUSR2) {
-      printf("\n+++ Child : Received signal SIGUSR2 from parent...\n");
-      sleep(5);
+      printf("\n\t\t\t\t\t+++ Child : Received signal SIGUSR2 from parent...\n\n");
+   } else if (sig == SIGINT) {
+      printf("\n\t\t\t\t\t+++ Child : Received signal SIGINT from parent...\n\n");
+   } else if (sig == SIGKILL) {
+      printf("\n\t\t\t\t\t+++ Child : Received signal SIGKILL from parent...\n\n");
    }
 }
 
@@ -41,75 +61,71 @@ int main ()
                                                             /* Parent process */
       int t;
 
-      t = 5 + rand() % 5;
-      printf("\n+++ Parent: Going to sleep for %d seconds\n", t);
-      sleep(t);       /* Sleep for some time before sending a signal to child */
+      mysleep();      /* Sleep for some time before sending a signal to child */
 
-      t = 5 + rand() % 5;
+      t = 1 + rand() % 2;
       printf("+++ Parent: Going to send signal SIGUSR%d to child\n", t);
       kill(pid, (t == 1) ? SIGUSR1 : SIGUSR2);        /* Send signal to child */
 
-      t = 5 + rand() % 5;
-      printf("\n+++ Parent: Going to sleep for %d seconds\n", t);
-      sleep(t);            /* Sleep for some time before suspending the child */
+      mysleep();           /* Sleep for some time before suspending the child */
       printf("+++ Parent: Going to suspend child\n");
       kill(pid, SIGTSTP);
       
-      t = 5 + rand() % 5;
-      printf("\n+++ Parent: Going to sleep for %d seconds\n", t);
-      sleep(t);             /* Sleep for some time before waking up the child */
+      mysleep();            /* Sleep for some time before waking up the child */
       printf("+++ Parent: Going to wake up child\n");
       kill(pid, SIGCONT);
       
-      t = 5 + rand() % 5;
-      printf("\n+++ Parent: Going to sleep for %d seconds\n", t);
-      sleep(t);             /* Sleep for some time before waking up the child */
-      printf("+++ Parent: Going to wake up child\n");
-      kill(pid, SIGCONT);
-      
-      t = 5 + rand() % 5;
-      printf("\n+++ Parent: Going to sleep for %d seconds\n", t);
-      sleep(t);            /* Sleep for some time before terminating the child */
-      printf("+++ Parent: Going to terminate child\n");
+      mysleep();  /* Sleep for some time before trying to terminate the child */
+      printf("+++ Parent: Trying to terminate child by SIGINT\n");
       kill(pid, SIGINT);
+
+      mysleep();  /* Sleep for some time before trying to terminate the child */
+      if (waitpid(pid, NULL, WNOHANG) == 0)
+         printf("+++ Parent: Could not terminate child\n");
+
+      printf("+++ Parent: Trying to terminate child by SIGKILL\n");
+      kill(pid, SIGKILL);
 
       /* Child is terminated, but the parent has not yet wait()-ed for it.    */
       /* So the child is now a zombie process. ps af will show this           */
       /* STAT Z and <defunct>.                                                */
       
-      t = 5 + rand() % 5;
-      printf("\n+++ Parent: Going to sleep for %d seconds\n", t);
-      sleep(t);               /* Sleep for some time before waiting for child */
+      mysleep();              /* Sleep for some time before waiting for child */
       waitpid(pid, NULL, 0);                        /* Wait for child to exit */
       printf("+++ Parent: Child exited\n");
 
       /* Child is now completely removed from the system as ps af will show.  */
 
-      t = 5 + rand() % 5;                           
-      printf("\n+++ Parent: Going to sleep for %d seconds\n", t);
-      sleep(t);                         /* Sleep for some time before exiting */
+      mysleep();                        /* Sleep for some time before exiting */
 
    } else {
                                                              /* Child process */
 
       /* The child will receive several signals from its parent. The first    */
       /* signal is randomly chosen between SIGUSR1 abd SIGUSR2. The default   */
-      /* handlers of these signals woul terminate the child. But the child    */
+      /* handlers of these signals would terminate the child. But the child   */
       /* refuses to die by registering the following custom-made handler      */
       /* for these two signals.                                               */
+
+      /* The child also tries to disable the following termination signals.   */
+      /* SIGINT  : Interrupt and terminate a process (same as ^C from user)   */
+      /* SIGKILL : Kill a process (cannot be caught)                          */
       signal(SIGUSR1, childSigHandler);           /* Register SIGUSR1 handler */
       signal(SIGUSR2, childSigHandler);           /* Register SIGUSR2 handler */
+      signal(SIGINT,  childSigHandler);           /* Register SIGINT  handler */
 
-      /* The child will also receive the following signals. These are not     */
-      /* handled by specific handler routines, so the default signal handling */
-      /* will take place.                                                     */
+      signal(SIGKILL, childSigHandler);           /* Register SIGKILL handler */
+                                                  /* No warning, no effect    */
+
+      /* The child will also receive the following signals from the parent.   */
+      /* The child does not register signal handlers for these signals,       */
+      /* so the default actions will be taken for these signals.              */
       /* SIGTSTP : Suspend a process (same as ^Z sent to the child)           */
       /* SIGCONT : Make a suspended process alive again                       */
-      /* SIGINT  : Interrupt and terminate a process (same as ^C from user)   */
 
-      while (1) sleep(1);     /* Sleep until a signal is received from parent */
+      while (1) pause();      /* Pause until a signal is received from parent */
 
-      /* Eventually the child is terminated by SIGINT from parent.            */
+      /* Eventually the child is terminated by SIGKILL from parent.           */
 
    }
 
